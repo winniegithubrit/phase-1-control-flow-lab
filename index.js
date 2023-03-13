d96aa6c0a6ac2afd46ffae8014fd62fd188cfe21
winniegithubrit <winnie.jomo@student.moringaschool.com>
2023-03-13 15:00:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let message;
+  if (feet <= 400) {
+    message = "This one is on me!";
+  
+  }else if (feet > 2000 && feet <= 2500){
+    message = "I will gladly take your thirty bucks.";
+  }else {
+    message = "No can do.";
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(destinationCity){
   // Write your code here!
+  return destinationCity === "NYC" ? "Ok, sounds good." : "No go.";
 }
+function switchOnCharmFromTip(tip) {
+  let message;
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (tip === 'generous') {
+    message = 'Thank you so much.';
+  } else if (tip === 'not as generous') {
+    message = 'Thank you.';
+  } else {
+    message = 'Bye.';
+  }
+
+  return message;
 }
